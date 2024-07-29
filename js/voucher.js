6f96896c92b2774798fed6018361085349d5ffd0
Kamil Jakacki <kamil@zapytania.com>
2024-07-29 18:39:17 +0000

    version bump 0

---
+++ js/voucher.js
@@ -0,0 +1,15 @@
+function NattakanVoucher() {
+    let that = this;
+
+    this.id = '';
+
+    this.init = function() {
+
+    }
+}
+let nattakanVoucher;
+
+jQuery(document).ready(function(){
+    nattakanVoucher = new NattakanVoucher();
+    nattakanVoucher.init();
+});
